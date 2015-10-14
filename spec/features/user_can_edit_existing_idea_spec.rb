e392fbb2cc20393ad0c3ac4dc897051387fdd88d
Rick Bacci <rickbacci@gmail.com>
2015-10-13 18:24:26 -0600

    add testing frameworks

--- spec/features/user_can_edit_existing_idea_spec.rb
+++ spec/features/user_can_edit_existing_idea_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 feature "User" do
 
-
-  scenario "can create an idea", js: true do
+  scenario "can edit an idea", js: true do
+    Idea.create(title: 'test idea', body: 'test idea body')
     visit root_path
+    find('a').click
 
     fill_in 'title', with: 'Idea title'
     fill_in 'body', with: 'Idea body'
 
-    click_on "Create Idea"
+    click_on "Update Idea"
+
 
     expect(page).to have_content('Idea title')
     expect(page).to have_content('Idea body')
   end
-
 end
 
---
+++ spec/test_spec.js
@@ -0,0 +1,10 @@
+//= require jquery
+//
+describe("My great feature", function() {
+
+  it("will change the world", function() {
+    expect(true).to.be(true);
+    expect(jQuery).to.not.be(undefined);
+  });
+
+});
