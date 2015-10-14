4b3832fa5c68843eaf08048442f6ca0ea3414ceb
Rick Bacci <rickbacci@gmail.com>
2015-10-13 19:18:31 -0600

    add edit and update controller actions

--- spec/features/user_can_edit_existing_idea_spec.rb
+++ spec/features/user_can_edit_existing_idea_spec.rb
@@ -5,13 +5,13 @@ feature "User" do
   scenario "can edit an idea", js: true do
     Idea.create(title: 'test idea', body: 'test idea body')
     visit root_path
-    find('a').click
 
-    fill_in 'title', with: 'Idea title'
-    fill_in 'body', with: 'Idea body'
+    find('.edit-idea').click
+
+    fill_in 'Title', with: 'Idea title'
+    fill_in 'Body', with: 'Idea body'
 
     click_on "Update Idea"
-
 
     expect(page).to have_content('Idea title')
     expect(page).to have_content('Idea body')
