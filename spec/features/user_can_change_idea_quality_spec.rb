67268fae7254d4ec25431f9fa040b05cb92eb2d9
Rick Bacci <rickbacci@gmail.com>
2015-10-15 00:42:09 -0600

    add feature tests for thumbs up and down

--- spec/features/user_can_change_idea_quality_spec.rb
+++ spec/features/user_can_change_idea_quality_spec.rb
@@ -3,25 +3,57 @@ require 'rails_helper'
 feature "User" do
 
   scenario "can increase the quality of an idea", js: true do
+    Idea.create(title: 'test idea', body: 'test idea body')
     visit root_path
 
     expect(page).to have_content('swill')
 
-    find('.thumbs-up').click
+    # page.execute_script('$("a.glyphicon-thumbs-up").click()')
+    page.find("a.glyphicon-thumbs-up").trigger("click")
 
+    visit root_path
     expect(page).to_not have_content('swill')
     expect(page).to have_content('plausible')
 
-    find('.thumbs-up').click
+    # page.execute_script('$("a.glyphicon-thumbs-up").click()')
+    page.find("a.glyphicon-thumbs-up").trigger("click")
 
     expect(page).to_not have_content('plausible')
     expect(page).to have_content('genius')
 
-    find('.thumbs-up').click
+    # page.execute_script('$("a.glyphicon-thumbs-up").click()')
+    page.find("a.glyphicon-thumbs-up").trigger("click")
 
     expect(page).to_not have_content('swill')
     expect(page).to_not have_content('plausible')
     expect(page).to have_content('genius')
+  end
+
+
+  scenario "can decrease the quality of an idea", js: true do
+    Idea.create(title: 'test idea', body: 'test idea body', quality: 2)
+    visit root_path
+
+    expect(page).to have_content('genius')
+
+    # page.execute_script('$("a.glyphicon-thumbs-up").click()')
+    page.find("a.glyphicon-thumbs-down").trigger("click")
+
+    expect(page).to_not have_content('genius')
+    expect(page).to have_content('plausible')
+
+    # page.execute_script('$("a.glyphicon-thumbs-down").click()')
+    page.find("a.glyphicon-thumbs-down").trigger("click")
+
+    expect(page).to_not have_content('plausible')
+    expect(page).to have_content('swill')
+
+    # page.execute_script('$("a.glyphicon-thumbs-down").click()')
+    page.find("a.glyphicon-thumbs-down").trigger("click")
+
+    expect(page).to have_content('swill')
+    expect(page).to_not have_content('plausible')
+    expect(page).to_not have_content('genius')
   end
 end
 
