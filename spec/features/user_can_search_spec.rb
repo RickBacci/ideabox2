29197324dc015213fb67ccac34ec5a17523b5fa9
Rick Bacci <rickbacci@gmail.com>
2015-10-14 19:02:37 -0600

    update idea search spec

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -3,22 +3,36 @@ require 'rails_helper'
 feature "User" do
 
   scenario "can search for an idea", js: true do
-    Idea.create(title: 'test idea1', body: 'test idea body1')
-    Idea.create(title: 'test idea2', body: 'test idea body2')
-    Idea.create(title: 'test idea3', body: 'test idea body3')
+    Idea.create(title: 'test idea1', body: 'test idea body')
+    # Idea.create(title: 'test idea2', body: 'test idea body2')
+    # Idea.create(title: 'test idea3', body: 'test idea body3')
+    Idea.create(title: '123', body: 'abc')
+    Idea.create(title: 'abc', body: '123')
 
     visit root_path
 
     expect(page).to have_content('test idea1')
-    expect(page).to have_content('test idea2')
-    expect(page).to have_content('test idea3')
+    expect(page).to have_content('123')
+    expect(page).to have_content('abc')
 
 
     fill_in 'Search', with: '1'
 
     expect(page).to have_content('test idea1')
-    expect(page).to_not have_content('test idea2')
-    expect(page).to_not have_content('test idea3')
+    expect(page).to have_content('123')
+    expect(page).to have_content('abc')
+
+    fill_in 'Search', with: '123'
+
+    expect(page).to have_content('123')
+    expect(page).to have_content('abc')
+    expect(page).to_not have_content('test idea1')
+
+    fill_in 'Search', with: '1234'
+
+    expect(page).to_not have_content('123')
+    expect(page).to_not have_content('abc')
+    expect(page).to_not have_content('test idea1')
   end
 end
 
